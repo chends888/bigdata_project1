ba98297f9242a233246539258f56b02e76dc8b6b
chends888 <lchends@gmail.com>
2019-04-02 09:37:09 -0300

    Progresso em adicionar itens às entidades, e conexão com bd aprimorada

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 import pymysql
+import functools
 from flask import jsonify, request
 import flask
 
@@ -6,37 +7,76 @@ import flask
 #local imports
 from app import app
 
-class ConnectionHelper:
-    def __init__(self, connection):
-        self.connection = connection
-    
-    def run(self, query):
+
+class MySqlConn:
+    def __init__(self):
+        connection_options = {
+            'host': 'localhost',
+            'user': 'chends',
+            'password': '8888',
+            'database': 'dharmadb'}
+        self.connection = pymysql.connect(**connection_options)
+
+    def run(self, query, args=None):
         with self.connection.cursor() as cursor:
             print('Executing query:')
-            print(cursor.mogrify(query))
-            cursor.execute(query)
+            print(cursor.mogrify(query, args))
+            cursor.execute(query, args)
             return cursor.fetchall()
 
-connection_options = {
-    'host': 'localhost',
-    'user': 'chends',
-    'password': '8888',
-    'database': 'dharmadb',
-}
-connection = pymysql.connect(**connection_options)
-db = ConnectionHelper(connection)
+
 
 @app.route('/')
-def index():
-    testquery = db.run('SELECT * FROM person')
+def select():
+    sqlconn = MySqlConn()
+    testquery = sqlconn.run('SELECT * FROM employee;')
+    sqlconn.connection.close()
     return jsonify(testquery)
 
-@app.route('/index', methods=['GET', 'POST'])
-def index2():
-    if flask.request.method == 'GET':
-        testquery = db.run('SELECT * FROM employee')
-        return jsonify(testquery)
-    else:
-        testquery = db.run('INSERT INTO employee (empname) VALUES ('+request.args.get('arg1')+')')
-        args = request.args.get('arg1')
-        return args
+@app.route('/addemp', methods=['POST'])
+def insertemp():
+    empname = request.args.get('empname')
+
+    sqlconn = MySqlConn()
+    testquery = sqlconn.run('INSERT INTO employee (empname) VALUES ("%s");' %(empname))
+    sqlconn.connection.commit()
+    sqlconn.connection.close()
+    return jsonify(testquery)
+
+@app.route('/addcat', methods=['POST'])
+def insertcat():
+    catname = request.args.get('catname')
+
+    sqlconn = MySqlConn()
+    testquery = sqlconn.run('INSERT INTO category (catname) VALUES ("%s");' %(catname))
+    sqlconn.connection.commit()
+    sqlconn.connection.close()
+    return jsonify(testquery)
+
+@app.route('/addproj', methods=['POST'])
+def insertproj():
+    projname = request.args.get('projname')
+    respid = request.args.get('respid')
+    catid = request.args.get('catid')
+
+    sqlconn = MySqlConn()
+    testquery = sqlconn.run('INSERT INTO project (projname, respid, catid) VALUES ("%s", %d, %d);' %(projname, respid, catid))
+    sqlconn.connection.commit()
+    sqlconn.connection.close()
+    return jsonify(testquery)
+
+@app.route('/addwork', methods=['POST'])
+def insertwork():
+    empid = request.args.get('empid')
+    projid = request.args.get('projid')
+
+    sqlconn = MySqlConn()
+    testquery = sqlconn.run('INSERT INTO works (empid, projid) VALUES (%d, %d);' %(empid, projid))
+    sqlconn.connection.commit()
+    sqlconn.connection.close()
+    return jsonify(testquery)
+
+
+
+
+
--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 import requests
 
-res = requests.post('http://127.0.0.1:5000/index?arg1=Johnny')
+res = requests.post('http://127.0.0.1:5000/addemp?name=Jo')
 restext = res.text
 print(restext)
 print(res)
